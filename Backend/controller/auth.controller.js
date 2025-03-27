84507412ba574dddc566c0d3f8f71cff940bc53a
zahopay <zahopay@gmail.com>
2025-03-27 21:09:49 +0530

    Update auth.controller.js

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -42,9 +42,11 @@ export const register = async (req, res) => {
 
     res.cookie("accessid", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: none,
       maxAge: 7 * 24 * 60 * 60 * 1000,
+      domain: '.onrender.com',
+      path: '/',  
     });
 
     //sending mail otp
@@ -94,9 +96,11 @@ export const login = async (req, res) => {
 
     res.cookie("accessid", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: none,
       maxAge: 7 * 24 * 60 * 60 * 1000,
+      domain: '.onrender.com',
+      path: '/',  
     });
 
     return res.json({
@@ -113,9 +117,11 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("accessid", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: none,
       maxAge: 0,
+      domain: '.onrender.com',
+      path: '/',  
     });
 
     return res.json({ success: true, message: "Logged Out" });
