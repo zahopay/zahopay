743c1b41621920cdc7ab5202f0e6f1241cbafaf7
zahopay <zahopay@gmail.com>
2025-03-29 17:02:54 +0530

    Update admin.adminAuth.js

--- Backend/middleware/admin.adminAuth.js
+++ Backend/middleware/admin.adminAuth.js
@@ -14,13 +14,6 @@ const adminAuth = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
-    if (decoded.role !== "admin") {
-      return res.status(403).json({
-        success: false,
-        message: "Admin privileges required"
-      });
-    }
-
     req.admin = { id: decoded.id };
     next();
     
