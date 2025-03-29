be546d291d32318a1b4ba71a991061dfed23e175
zahopay <zahopay@gmail.com>
2025-03-29 16:28:02 +0530

    Update AppContext.jsx

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -30,15 +30,6 @@ export const AppContextProvider = ({ children }) => {
     isLoading: true,
   });
 
-    const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-
 
   const verifyAuth = async () => {
     try {
