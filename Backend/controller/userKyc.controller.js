8588dea20f8cc6049e4d80a5974c8f9142a728dd
zahopay <zahopay@gmail.com>
2025-03-30 20:41:13 +0530

    Update userKyc.controller.js

--- Backend/controller/userKyc.controller.js
+++ Backend/controller/userKyc.controller.js
@@ -11,27 +11,24 @@ export const uploadUserKyc = async(req, res) => {
             return res.json({success : false, message : "Login Again"})
         }
 
-        const host = req.get("host");
-        const protocol = req.protocol;
-
-        const aadharFrontImage = req.files["aadharFrontImage"]
-          ? `${protocol}://${host}/${req.files[
-              "aadharFrontImage"
-            ][0].path.replace(/\\/g, "/")}`
-          : null;
+const protocol = req.headers['x-forwarded-proto'] || req.protocol;
+const host = req.get('host');
 
 
-        const aadharBackImage = req.files["aadharBackImage"]
-          ? `${protocol}://${host}/${req.files[
-              "aadharBackImage"
-            ][0].path.replace(/\\/g, "/")}`
-          : null;
-        const panImage = req.files["panImage"]
-          ? `${protocol}://${host}/${req.files["panImage"][0].path.replace(
-              /\\/g,
-              "/"
-            )}`
-          : null;
+const generateSecureImageUrl = (file) => {
+  if (!file) return null;
+  
+  // Remove any absolute server path and normalize slashes
+  const relativePath = file[0].path
+    .replace(/^.*[\\\/]uploads[\\\/]/, '') // Remove any leading path up to /uploads
+    .replace(/\\/g, '/');
+    
+  return `https://${host}/uploads/${relativePath}`; // Force HTTPS
+};
+
+        const aadharFrontImage = generateSecureImageUrl(req.files['aadharFrontImage']);
+        const aadharBackImage = generateSecureImageUrl(req.files['aadharBackImage']);
+        const panImage = generateSecureImageUrl(req.files['panImage']);
 
 
           if(!aadharBackImage || !aadharFrontImage || !panImage){
